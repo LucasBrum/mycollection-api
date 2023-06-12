87f899af796f1d534c813428b2f68c429dfa49cf
lucasbrum <brum.lucas@icloud.com>
2023-06-12 05:49:36 -0300

    Adding loggin messages in Category Service

--- src/main/java/com/brum/mycollection/api/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/brum/mycollection/api/service/impl/CategoryServiceImpl.java
@@ -6,6 +6,7 @@ import com.brum.mycollection.api.model.request.CategoryRequest;
 import com.brum.mycollection.api.model.response.CategoryResponse;
 import com.brum.mycollection.api.repository.CategoryRepository;
 import com.brum.mycollection.api.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
     private final ModelMapper mapper;
@@ -28,81 +30,71 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryResponse create(CategoryRequest categoryRequest) {
-        try {
-            Category category = this.mapper.map(categoryRequest, Category.class);
+        log.info("Criando Categoria {}.", categoryRequest.getName());
+        Category category = this.mapper.map(categoryRequest, Category.class);
 
-            validateCategoryName(categoryRequest);
-            verifyIfCategoryNameExists(categoryRequest);
+        validateCategoryName(categoryRequest);
+        verifyIfCategoryNameExists(categoryRequest);
 
-            this.categoryRepository.save(category);
-            var categoryResponse = mapper.map(category, CategoryResponse.class);
-            return categoryResponse;
-        } catch (CategoryException cex) {
-            throw cex;
-        } catch (Exception e) {
-            throw new CategoryException("Erro interno", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        this.categoryRepository.save(category);
+        log.info("Categoria {} salva com sucesso!", category.getName());
+        var categoryResponse = mapper.map(category, CategoryResponse.class);
+        return categoryResponse;
     }
 
     @Override
     public CategoryResponse update(Long id, CategoryRequest categoryRequest) {
+        log.info("Atualizando Categoria {}.", categoryRequest.getName());
+
         Category category = this.mapper.map(categoryRequest, Category.class);
         this.findById(id);
-        try {
-            category.setId(id);
-            this.categoryRepository.save(category);
-            var categoryResponse = mapper.map(category, CategoryResponse.class);
+        category.setId(id);
+        this.categoryRepository.save(category);
 
-            return categoryResponse;
-        } catch (Exception e) {
-            throw new CategoryException("Erro interno.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        var categoryResponse = mapper.map(category, CategoryResponse.class);
+        log.info("Categoria atualizada com sucesso!");
+        return categoryResponse;
     }
 
     @Override
     public CategoryResponse findById(Long id) {
-        try {
-            Optional<Category> category = this.categoryRepository.findById(id);
-            if (category.isPresent()) {
-                CategoryResponse categoryResponse = mapper.map(category.get(), CategoryResponse.class);
-                return categoryResponse;
-            }
+        log.info("Buscando categoria...");
+        Optional<Category> category = this.categoryRepository.findById(id);
 
+        if (category.isEmpty()) {
             throw new CategoryException("Categoria não encontrada.", HttpStatus.NOT_FOUND);
-
-        } catch (CategoryException cex) {
-            throw cex;
-        } catch (Exception e) {
-            throw new CategoryException("Erro interno.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
+        CategoryResponse categoryResponse = mapper.map(category.get(), CategoryResponse.class);
+        log.info("Categoria {} encontrada.", category.get().getName());
+        return categoryResponse;
     }
 
     @Override
     public List<CategoryResponse> list() {
-        try {
-            List<Category> categories = this.categoryRepository.findAllByOrderByNameAsc();
-            return this.mapper.map(categories, new TypeToken<List<CategoryResponse>>() {}.getType());
-        } catch (Exception e) {
-            throw new CategoryException("Erro interno.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        log.info("Listando categorias...");
+        List<Category> categories = this.categoryRepository.findAllByOrderByNameAsc();
+        return this.mapper.map(categories, new TypeToken<List<CategoryResponse>>() {
+        }.getType());
+
     }
 
     @Override
     public void delete(Long id) {
-        try {
-            this.findById(id);
+        CategoryResponse categoriaResponse = this.findById(id);
 
-            this.categoryRepository.deleteById(id);
-        } catch (CategoryException ce) {
-            throw new CategoryException("Erro interno.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        log.info("Deletando categoria {} ...", categoriaResponse.getName());
+        this.categoryRepository.deleteById(id);
+        log.info("Categoria {} deletada com sucesso!", categoriaResponse.getName());
     }
 
     private void verifyIfCategoryNameExists(CategoryRequest categoryRequest) {
-        Boolean categoryExistsByName = categoryRepository.existsByName(categoryRequest.getName());
+        log.info("Validando se categoria {} existe.", categoryRequest.getName());
+        boolean categoryExistsByName = categoryRepository.existsByName(categoryRequest.getName());
         if (categoryExistsByName) {
             throw new CategoryException("Categoria já existe.", HttpStatus.CONFLICT);
         }
+        log.info("A categoria {} foi validada.", categoryRequest.getName());
     }
 
     private void validateCategoryName(CategoryRequest categoryRequest) {
