df73e58f72ea0986b07cde25fe3544d898e2c263
lucasbrum <brum.lucas@icloud.com>
2023-06-26 09:33:04 -0300

    Backup DB

---
+++ db-backup/backup-sql/artists_202306260929.sql
@@ -0,0 +1,21 @@
+INSERT INTO public.artists (id,country,name) VALUES
+	 (1,'Sweden','Leviticus'),
+	 (3,'Norway','Pagans Mind'),
+	 (5,'United States','Cacophony'),
+	 (6,'Germany','Axel Rudi Pell'),
+	 (7,'Canada','Borealis'),
+	 (8,'United States','The Human Abstract'),
+	 (9,'United States','Impellitteri'),
+	 (10,'United States','Further Seems Forever'),
+	 (12,'United States','One Bad Pig'),
+	 (13,'Italy','DGM');
+INSERT INTO public.artists (id,country,name) VALUES
+	 (14,'Netherlands','Elegy'),
+	 (15,'Netherlands','Textures'),
+	 (16,'United States','Death'),
+	 (17,'Brazil','Dark Avenger'),
+	 (18,'Denmark','Pyramaze'),
+	 (19,'Norway','Jorn'),
+	 (11,'Italy','Thy Majestie'),
+	 (20,'Brazil','Angra'),
+	 (21,'United States','The Fall Of Troy');
